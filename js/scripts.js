394db3665f825cbb6877ce124fb4baadd1fc4923
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-19 10:08:20 -0700

    remove a function from js that I don't think is necessary

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@ function  Pizza(toppings, size,)  {
   this.toppings = toppings;
   this.size = size;
 }
+function Order()  {
+  this.price = 0;
+}
 
 Pizza.prototype.orderUp = function()  {
   return this.size + this.toppings + " is ready!";
 }
 
-function Order()  {
-  this.price = 0;
-}
 
 // User Interface
 $(document).ready(function()  {
@@ -18,7 +18,7 @@ $(document).ready(function()  {
     event.preventDefault();
     let inputtedSize = $("input#pizzaSize").val();
     let inputtedToppings =  $("input#toppings").val();
-    let newOrder = new Pizza(inputtedSize, inputtedToppings);
-    $("#order-output").text(newOrder);
+    // let newOrder = new Pizza(inputtedSize, inputtedToppings);
+    $("#order-output").text(addToTotal());
   })
 })
